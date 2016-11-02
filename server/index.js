640ba71dc0943e0909ece3352ca1271c5e27bc08
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-11-02 11:13:19 +0200

    Adds /api endpoint for loading records

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { logger, expressWinston } from 'server/logger';
 import { readEnvironmentVariable } from 'server/utils';
 import cookieParser from 'cookie-parser';
 import { sessionController } from 'server/session-controller';
+import { marcIOController } from 'server/marc-io-controller';
 import path from 'path';
 
 const PORT = readEnvironmentVariable('HTTP_PORT', 3001);
@@ -13,6 +14,7 @@ const app = express();
 app.use(expressWinston);
 app.use(cookieParser());
 
+app.use('/api', marcIOController);
 app.use('/session', sessionController);
 
 app.use(express.static(path.resolve(__dirname, 'public')));
