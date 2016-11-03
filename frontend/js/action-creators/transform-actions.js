83bee737f829c49cbbb3cb3d406c6b065be169e7
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-11-03 13:23:32 +0200

    Integrates the transliteration fn to transform-action

--- frontend/js/action-creators/transform-actions.js
+++ frontend/js/action-creators/transform-actions.js
@@ -1,4 +1,6 @@
 import { TRANSFORM_RECORD_ERROR, TRANSFORM_RECORD_SUCCESS } from '../constants/action-type-constants';
+import { transliterate } from 'transformations/transliterate';
+import MarcRecord from 'marc-record-js';
 
 export function transformRecordSuccess(recordId, record) {
   return { type: TRANSFORM_RECORD_SUCCESS, recordId, record };
@@ -9,6 +11,13 @@ export function transformRecordError(error) {
 
 export function transformRecord(recordId, record) {
   return function(dispatch) {
-    dispatch(transformRecordSuccess(recordId, record));
+
+    const copy = new MarcRecord(record);
+
+    transliterate(copy).then(transliteratedRecord => {
+      window.x = transliteratedRecord;
+      transliteratedRecord.fields.forEach((f,i) => f.uuid = i);
+      dispatch(transformRecordSuccess(recordId, transliteratedRecord));
+    });
   };
 }
