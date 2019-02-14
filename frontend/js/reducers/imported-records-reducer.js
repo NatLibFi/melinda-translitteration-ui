364db68ba34d0e5be480b72546d3bd58f68e5c75
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-02-14 11:51:22 +0200

    Update various stuff 1) Copyright year 2) Name 3) Fix travis 4) package-lock

--- frontend/js/reducers/imported-records-reducer.js
+++ frontend/js/reducers/imported-records-reducer.js
@@ -4,16 +4,16 @@
 *
 * UI for transliterating MARC records in Melinda
 *
-* Copyright (c) 2016-2017 University Of Helsinki (The National Library Of Finland)
+* Copyright (c) 2016-2019 University Of Helsinki (The National Library Of Finland)
 *
-* This file is part of melinda-transliteration-ui
+* This file is part of melinda-cyrillux
 *
-* melinda-transliteration-ui program is free software: you can redistribute it and/or modify
+* melinda-cyrillux program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
 *
-* melinda-transliteration-ui is distributed in the hope that it will be useful,
+* melinda-cyrillux is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU Affero General Public License for more details.
